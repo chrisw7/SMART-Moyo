8e09b4efcd63a90d5571c77a578cf6c0a991831d
Junaid Siddiqui <junaidas97@gmail.com>
2017-09-12 16:30:00 -0400

    variable change
    Changed 'a' to accel for clarity

--- src/activity.m
+++ src/activity.m
@@ -1,15 +1,15 @@
-function [COMPS] = activity(a)
+function [COMPS] = activity(accel)
 %activity Check for movement in an acceleration signal
 %   Check range/number of compressions, return true if motion detected
 %   ---
 %   Authour: Chris Williams | Last Updated: April 24, 2017
 %   McMaster University 2017
 
-if max(a) < 2 && min(a) > -2
+if max(accel) < 2 && min(accel) > -2
     COMPS = false;
 else
     
-    locs = segmentSignal(a,100);
+    locs = segmentSignal(accel,100);
     
     if length(locs)<=2
         COMPS = false;
