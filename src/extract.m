cb6a53b371acec0f82c0526d8ae3f36ee762ec28
Junaid Siddiqui <junaidas97@gmail.com>
2017-09-12 16:32:52 -0400

    variable names
    t to time and a to acceleration for clarity

--- src/extract.m
+++ src/extract.m
@@ -1,33 +1,39 @@
-function [T,A] = extract(N,port,offset)
-%extract Polls sensor connected to COM4 for time/acceleration data
-%   [T,A] = extract(N,port,offset) extracts time in seconds (T) and z-acc. 
-%   (m/s/s) given a # of samples, N, port identifier ('COM4'), and raw
-%   offset value
-%   ---
-%   Authour: Chris Williams | Last Updated: April 26, 2017
-%   McMaster University 2017
+function [time, accel] = extract(numSamples,PORT,offset)
+    %extract Polls sensor connected to COM4 for time/acceleration data
+    %   [T,A] = extract(N,port,offset) extracts time in seconds (T) and z-acc. 
+    %   (m/s/s) given a # of samples, N, port identifier ('COM4'), and raw
+    %   offset value
+    %   ---
+    %   Authour: Chris Williams | Last Updated: April 26, 2017
+    %   McMaster University 2017
 
-%Open serial communications
-imu = serial(port,'BaudRate',115200);
-fopen(imu);
+    GRAVITY = 9.80665;
 
-%Poll sensor
-y = zeros(N,4);
-fprintf('Polling IMU on %s...\n', port)
-for i=1:N
-        y(i,:) = eval( [ '[', fscanf(imu), ']' ] );
-end
+    %Open serial communications
+    imu = serial(PORT,'BaudRate',115200);
+    fopen(imu);
 
-%Scale data
-T = y(:,1);
-T = (T-T(1))/1000;
+    %Poll sensor
+    y = zeros(numSamples,4);
+    fprintf('Polling IMU on %s...\n', PORT)
+    for i=1:numSamples
+            %Debugging
+            fprintf(fscanf(imu))
+            fprintf(eval( [ '[', fscanf(imu), ']' ] ))
 
-A = y(:,4);
-A = (A - offset)*9.80665;
+            y(i,:) = eval( [ '[', fscanf(imu), ']' ] );
+    end
 
-%Close serial communications
-fclose(instrfind);
+    %Scale data
+    time = y(:,1);
+    time = (time-time(1))/1000;
 
-fprintf('Data capture complete\n')
-% beep;
+    accel = y(:,4);
+    accel = (accel - offset)*GRAVITY;
+
+    %Close serial communications
+    fclose(instrfind);
+
+    fprintf('Data capture complete\n')
+    % beep;
 end
