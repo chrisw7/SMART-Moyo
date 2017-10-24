64e5e15ae355e82c34657a857921637b0da48426
Junaid Siddiqui <junaidas97@gmail.com>
2017-10-24 11:33:32 -0400

    Dependencies for windows

--- srcpy/scripts.bat
+++ srcpy/scripts.bat
@@ -1,15 +1,10 @@
 cd C:\WINDOWS\system32
 python -m pip install -U pip
-py -m pip install -U pip
 
 python -m pip install matplotlib
-py -m pip install matplotlib
 
 python -m pip install peakutils
-py -m pip install peakutils
 
 python -m pip install numpy
-py -m pip install numpy
 
 python -m pip install serial
-py -m pip install serial
