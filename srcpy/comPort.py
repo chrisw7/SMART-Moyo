865125ef212e8b0742fb015a9c78bc8653485ab7
junaidasiddiqui <junaidas97@gmail.com>
2017-10-26 16:54:00 -0400

    Dynamic Port assignments

--- srcpy/comPort.py
+++ srcpy/comPort.py
@@ -1,5 +1,38 @@
-import serial
 import io
+import glob
+import serial
+import sys
+
+
+""" Lists serial port names
+
+    :raises EnvironmentError:
+        On unsupported or unknown platforms
+    :returns:
+        A list of the serial ports available on the system
+"""
+def serial_ports():
+
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result[0]
+
 
 #Checks for idle accelerometer (No compressions being performed)
 def idle(accel, err):
@@ -15,8 +48,7 @@ def openSerial(port, baud):
     ser = serial.Serial(port, baud)
 
     if ser.isOpen():
-        print(ser)
-        print(ser.name + ' is open')
+        print('Port ' + ser.name + ' is open')
     else:
         print("Port is not open")
         ser.close()
@@ -27,8 +59,7 @@ def openSerial(port, baud):
 #Reads one line from the serial port
 def readSerial(port, byte):
     ser = serial.Serial(port)
-    data = ser.read(byte).decode("utf-8").replace("\r\n","")
-
+    data = ser.read(byte).decode("utf-8")
     return data
 
 def openBluetooth(port, baud):
