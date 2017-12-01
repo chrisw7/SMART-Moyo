810b02c5db4a29c79b30558000be0cdd521b6109
Junaid Siddiqui <junaidas97@gmail.com>
2017-12-01 13:22:03 -0500

    Update scripts.bat

--- pysrc/scripts.bat
+++ pysrc/scripts.bat
@@ -10,3 +10,15 @@ python -m pip install numpy
 python -m pip install pyserial
 
 python -m pip install serial
+
+py -m pip install -U pip
+
+py -m pip install matplotlib
+
+py -m pip install peakutils
+
+py -m pip install numpy
+
+py -m pip install pyserial
+
+py -m pip install serial
